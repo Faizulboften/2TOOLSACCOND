99a72bf1979479651f84ef442e5a676d7c0eded1
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-04 13:57:37 +0800

    Create 3acc.sh

---
+++ 3acc.sh
@@ -0,0 +1,30 @@
+
+
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+echo $me "
+╭━┳━╭━╭━╮%s╮╲╲╲╲╲╲%sFAJZUL BOFTEN
+┃┈┈┈┣▅╋▅┫┃%s╲╲╲╲╲╲%s3 SCRIP TOOLS BERGUNA
+┃┈┃┈╰━╰━━━━━━╮%s╲╲%s🔴NOTE RECODE
+╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣%s╲%s🔴INDONESIA
+╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉%s╲%s🔴SILTENG
+╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤%s╲%s🔴AMPANA KOTA
+╲┃┈┈┈┈╭━┳━━━━╯%s╲╲%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 :082271426251
+╲┣━━━━━━┫%s╲╲╲╲╲╲╲%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
+╲┃┈┈┈┈┈┈┃%s╲╲╲╲╲╲╲%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩ "
+
+echo $or "
+echo $cyan "🔓 MENGGINSTALL SERVER"
+sleep 20
+echo $cyan "🔓 MASUK "
+sleep 10
+
+termux-open https://soo.gd/qtNdP
