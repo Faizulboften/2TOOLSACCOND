f77ae82ee92e09bbed9c1f52cd5b8e951808332a
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-04 14:02:56 +0800

    Update 3acc.sh

--- 3acc.sh
+++ 3acc.sh
@@ -10,7 +10,7 @@ pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
 or='\033[1;38;5;208m' #Orange
 
-echo $ij "
+echo $or "
 ╭━┳━╭━╭━╮%s╮╲╲╲╲╲╲%sFAJZUL BOFTEN
 ┃┈┈┈┣▅╋▅┫┃%s╲╲╲╲╲╲%s3 SCRIP TOOLS BERGUNA
 ┃┈┃┈╰━╰━━━━━━╮%s╲╲%s🔴NOTE RECODE
@@ -20,7 +20,7 @@ echo $ij "
 ╲┃┈┈┈┈╭━┳━━━━╯%s╲╲%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 :082271426251
 ╲┣━━━━━━┫%s╲╲╲╲╲╲╲%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
 ╲┃┈┈┈┈┈┈┃%s╲╲╲╲╲╲╲%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩ "
-sleep
+sleep 10
 echo $cyan "🔓 MENGGINSTALL SERVER"
 sleep 20
 echo $cyan "🔓 MASUK "
